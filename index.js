a71ee10260b7c613d7f5ccf1a879ccae44d89d32
CamilingJS <jonathancamiling@gmail.com>
2024-08-18 20:56:16 -0700

    added trim and alt case str No Data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import * as cheerio from 'cheerio';
     const containers = $('.tileInnerContainer').toArray();
     const trends = containers.map(item => {
         const active = $(item)
-       const keyword = active.find('.tileTopInfoContainer > .tileKeywordContainer > .tileKeyword').text();
+       const keyword = active.find('.tileTopInfoContainer > .tileKeywordContainer > .tileKeyword').text().trim() || 'No data';
        const volumeScore = active.find('.tileTopInfoContainer .scoresContainer .scoresInnerContainer .scoreTag--volume').text().trim() || 'No Data';
        const growthScore = active.find('.tileTopInfoContainer .scoresContainer .scoresInnerContainer .scoreTag.last .growth').text().trim() || 'No Data';
        const description = active.find('.tileChartContainer .tileBottomInfoContainer .tileDescription').text().trim() || 'No Data';
